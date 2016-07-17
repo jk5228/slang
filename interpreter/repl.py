08805f07defb4c5424ce24738e3c6c2227711d92
Jason Kim <jasonkim.0815@gmail.com>
2016-07-17 14:27:57 -0700

    Refactor repl.py using cmd module

--- interpreter/repl.py
+++ interpreter/repl.py
@@ -4,56 +4,75 @@
 
 import sys
 import env
+import cmd
 import copy
 import slang
 import colors
 import execute
 
-# The help string
-help_str = '''Commands:
-exit    : Exit from the REPL.
-locals  : Print all of the bindings in the environment.
-reset   : Reset the environment to its initial set of bindings.
-help    : Print this list of commands.
-'''
+class repl(cmd.Cmd):
 
-# Launch the RELP.
-def launch():
+    intro = 'Slang REPL\nEnter "exit" to quit.\nEnter "help" for more commands.'
     fresh_env = copy.deepcopy(env.env)
     in_prompt = '%s%s%s' % (colors.color('In  [', colors.OKGREEN), '%s', colors.color(']: ', colors.OKGREEN))
     out_prompt = '%s%s%s%s' % (colors.color('Out [', colors.FAIL), '%s', colors.color(']: ', colors.FAIL), '%s')
+    prompt = ''
+    ret_prompt = ''
     cnt = 0
-    print('Slang REPL')
-    print('Enter "exit" to quit.')
-    print('Enter "help" for more commands.')
-    print()
-    sys.stdout.write(in_prompt % cnt)
-    sys.stdout.flush()
-    for line in sys.stdin:
-        line = line.strip()
-        if not line:
-            sys.stdout.write(in_prompt % cnt)
-            sys.stdout.flush()
-            continue
-        if line == 'exit': break
-        elif line == 'locals':
-            print('Environment:')
-            for (key, val) in env.env.items():
-                print('%s\t=\t%s' % (key, val))
-            print()
-        elif line == 'reset':
-            env.env = fresh_env
-            print('Environment reset.\n')
-        elif line == 'help':
-            print(help_str)
-        else:
-            try:
-                res = slang.run(line)
-                res = res.res
-                print(out_prompt % (cnt, str(res)))
-            except Exception as err:
-                print(colors.color('Error: %s', colors.FAIL) % err)
 
-        cnt += 1
-        sys.stdout.write(in_prompt % cnt)
-        sys.stdout.flush()
+    # Utility functions
+
+    # Update the in_prompt with the count number.
+    def update_in_prompt(self):
+        self.prompt = self.in_prompt % self.cnt
+
+    # Update the out_prompt with the count number.
+    def update_out_prompt(self):
+        self.ret_prompt = self.out_prompt % self.cnt
+
+    # Read in Slang line. This is the default action.
+    def default(self, line):
+        try:
+            res = slang.run(line)
+            res = res.res
+            print(self.out_prompt % (self.cnt, str(res)))
+        except Exception as err:
+            print(colors.color('Error: %s', colors.FAIL) % err)
+        self.cnt += 1
+
+    # Commands
+
+    def do_exit(self, arg):
+        'Exit from the REPL.'
+        exit(0)
+
+    def do_locals(self, arg):
+        'Print all of the bindings in the environment.'
+        print('Environment:')
+        for (key, val) in env.env.items():
+            print('%s\t=\t%s' % (key, val))
+        print()
+
+    def do_reset(self, arg):
+        'Reset the environment to its initial set of bindings.'
+        env.env = self.fresh_env
+        print('Environment reset.\n')
+
+    # Other hook methods.
+
+    # Get initial prompt.
+    def preloop(self):
+        self.update_in_prompt()
+
+    # Ignore empty lines.
+    def emptyline(self):
+        pass
+
+    # Update the prompt.
+    def postcmd(self, stop, line):
+        # TODO: update cnt
+        self.update_in_prompt()
+
+# Launch the RELP.
+def launch():
+    repl().cmdloop()
