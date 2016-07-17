f96658df313d0e52e1979c92bc18911fd05dbd82
Jason Kim <jasonkim.0815@gmail.com>
2016-07-17 00:36:13 -0700

    Fix bug allowing any type to be added to a string

--- interpreter/execute.py
+++ interpreter/execute.py
@@ -193,7 +193,7 @@ def evaluate(envs, exp):
 
         if match(op, '+'):                  # Add
             # print('-> +')
-            if string in (type(left), type(right)):
+            if string in (type(left), type(right)) and number in (type(left), type(right)):
                 return string(str(unwrap(left)) + str(unwrap(right)))
             elif type(left) == number and type(right) == number:
                 return number(unwrap(left) + unwrap(right))
